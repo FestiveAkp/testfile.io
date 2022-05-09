99fdcb01e0c8625851018860705ecaa56984c8e2
Akash Pillai <akashp807@gmail.com>
2022-05-08 23:04:56 -0500

    Initialize lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to io.testfile.core
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/io/testfile/core.clj
@@ -0,0 +1,7 @@
+(ns io.testfile.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [_]
+  (println "Hello, World!"))
