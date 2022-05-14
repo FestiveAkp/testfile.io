b7d38af327bb394e0f075465f9f606e76acbc4ac
Akash Pillai <akashp807@gmail.com>
2022-05-13 23:56:34 -0500

    Handle runtime exceptions in random file handler

--- src/io/testfile/util.clj
+++ src/io/testfile/util.clj
@@ -13,18 +13,20 @@
   [human-string]
   (if (number? (as-int human-string))
     (as-int human-string) ; pass through if just an integer
-    (let [re-match (re-find #"([\d.,]+)\s*(\w+)" human-string)
-          magnitude (some-> re-match
-                            (get 1)
-                            (Double/parseDouble))
-          unit (some-> re-match
-                       (get 2)
-                       (upper-case))]
-      (if (nil? magnitude)
-        nil
-        (case unit
-          "GB" (Math/round (* magnitude 1073741824))
-          "MB" (Math/round (* magnitude 1048576))
-          "KB" (Math/round (* magnitude 1024))
-          "B" (Math/round magnitude)
-          nil)))))
+    (try
+      (let [re-match (re-find #"([\d.,]+)\s*(\w+)" human-string)
+            magnitude (some-> re-match
+                              (get 1)
+                              (Double/parseDouble))
+            unit (some-> re-match
+                         (get 2)
+                         (upper-case))]
+        (if (nil? magnitude)
+          nil
+          (case unit
+            "GB" (Math/round (* magnitude 1073741824))
+            "MB" (Math/round (* magnitude 1048576))
+            "KB" (Math/round (* magnitude 1024))
+            "B" (Math/round magnitude)
+            nil)))
+      (catch Exception _ nil))))
