4c2c32ce128161c5a8ef3fe1bde947b76cd43b4c
Akash Pillai <akashp807@gmail.com>
2022-07-20 15:39:32 -0500

    Remove unused directories

--- test/io/testfile/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns io.testfile.core-test
-  (:require [clojure.test :refer [deftest testing is]]
-            [io.testfile.core :refer []]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
