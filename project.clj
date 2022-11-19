a5b69fe59e343e45a32d0b14a35efdafa5443acb
Akash Pillai <akashp807@gmail.com>
2022-11-18 23:53:24 -0600

    Add logging

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.logging "1.2.4"]
+                 [ch.qos.logback/logback-classic "1.4.4"]
                  [ring/ring-core "1.9.5"]
                  [ring/ring-defaults "0.3.3"]
                  [ring/ring-jetty-adapter "1.9.5"]
--- src/io/testfile/core.clj
+++ src/io/testfile/core.clj
@@ -1,10 +1,27 @@
 (ns io.testfile.core
   (:require [ring.adapter.jetty :as ring]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [io.testfile.routes :refer [routes]])
+            [io.testfile.routes :refer [routes]]
+            [clojure.tools.logging :as log])
   (:gen-class))
 
-(def app (wrap-defaults #'routes api-defaults))
+(defn wrap-logging
+  "Middleware for logging requests to the console."
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (log/info {:uri              (get request :uri)
+                 :request-method   (get request :request-method)
+                 :content-type     (get-in request [:headers "content-type"])
+                 :content-encoding (get-in request [:headers "content-encoding"])
+                 :query-string     (get request :query-string)
+                 :http-status      (get response :status)
+                 :user-agent       (get-in request [:headers "user-agent"])})
+      response)))
+
+(def app (-> #'routes
+             wrap-logging
+             (wrap-defaults api-defaults)))
 
 (defonce server (atom nil))
 
@@ -18,5 +35,6 @@
     (reset! server nil)))
 
 (defn -main []
-  (let [port (Integer. (or (System/getenv "PORT") "8080"))]
-    (start-server port)))
+  (let [port (Integer. (or (System/getenv "PORT") "8080"))] 
+    (start-server port)
+    (log/info "Server initialized on port" port)))
